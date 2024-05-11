dea8cb0b98a84e65c182e5c1874b90e51039771a
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-11 04:47:03 -0500

    deprecate iattack

--- EverCraft/Attack.cs
+++ EverCraft/Attack.cs
@@ -1,20 +1,7 @@
 namespace EverCraft;
 
-public class Attack : IAttack
+public class Attack
 {
-    public ICharacter Attacker { get; init; }
-    private readonly ICharacter _defender;
-
-    public void PerformAttack(int roll)
-    {
-        if(_defender.LegacyAttack(roll))
-        {
-            Attacker.XP += 10;
-        }
-    }
-
-    public Attack(ICharacter attacker, ICharacter defender) => (Attacker, _defender) = (attacker, defender);
-
     public static void PerformAttack(ICharacter attacker, ICharacter defender, int roll)
     {
         if(defender.LegacyAttack(roll))
