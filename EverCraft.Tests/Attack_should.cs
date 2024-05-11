17f91c4e268859dace3a96ce710be2ad2cdb7a47
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-05-11 05:00:45 -0500

    moar parts of the theory

--- EverCraft.Tests/Attack_should.cs
+++ EverCraft.Tests/Attack_should.cs
@@ -5,7 +5,10 @@ public class Attack_should
 {
 
     [Theory]
+    [InlineData(1, 0)]
+    [InlineData(9, 0)]
     [InlineData(10, 10)]
+    [InlineData(11, 10)]
     public void reward_attacker_on_successful_attack(int roll, int expectedXP)
     {
         // Arrange
