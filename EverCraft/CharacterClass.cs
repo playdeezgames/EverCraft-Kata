c460c429d3818260758559fe5cdf013b78ecaea8
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-08 05:43:45 -0500

    character extensions

--- EverCraft/CharacterClass.cs
+++ EverCraft/CharacterClass.cs
@@ -7,16 +7,20 @@ public enum CharacterClass
     Rogue,
 }
 
-public static class CharacterClassExtensions 
+public static class CharacterExtensions
 {
     public static int CriticalDamageMultiplier(this ICharacter character) => character.CharacterClass.CriticalDamageMultiplier();
+}
+
+public static class CharacterClassExtensions 
+{
     public static int BaseHPModifierPerLevel(this CharacterClass characterClass) => characterClass switch 
     {
         CharacterClass.Fighter => 10,
         _ => 5,
     };
 
-    private static int CriticalDamageMultiplier(this CharacterClass characterClass) => characterClass switch
+    internal static int CriticalDamageMultiplier(this CharacterClass characterClass) => characterClass switch
     {
         CharacterClass.Rogue => 3,
         _ => 2,
