d8a011dba80b01231f2d24b387456d67a92158e8
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-08 05:31:02 -0500

    pushed down base damage

--- EverCraft/Attack.cs
+++ EverCraft/Attack.cs
@@ -4,7 +4,12 @@ public static class Attack
 {
     public static void PerformAttack(this ICharacter attacker, ICharacter defender, int roll)
     {
-        int damage = CalculateDamage(defender.CurrentArmurKlass, roll, attacker.AttackBonus(), attacker.GetAbilityScoreModifier(Ability.Strength), attacker.CharacterClass.CriticalAttackDamageMultiplier());
+        int damage = CalculateDamage(
+            defender.CurrentArmurKlass,
+            roll,
+            attacker.AttackBonus(),
+            (1 + attacker.GetAbilityScoreModifier(Ability.Strength)),
+            attacker.CharacterClass.CriticalAttackDamageMultiplier());
 
         if (damage > 0)
         {
@@ -19,9 +24,12 @@ public static class Attack
         _ => attacker.Level / 2,
     };
 
-    private static int CalculateDamage(int armurKlass, int roll, int attackBonus, int damageBonus = 0, int damageMultiplier = 2)
+    private static int CalculateDamage(
+        int armurKlass,
+        int roll, int attackBonus,
+        int baseDamage,
+        int damageMultiplier = 2)
     {
-        int baseDamage = 1 + damageBonus;
         if (roll == 20) { return baseDamage * damageMultiplier; }
         if (roll + attackBonus >= armurKlass) { return baseDamage; }
         return 0;
