eb35c13fa9faf054a226bc4ce9747a02e04c69e9
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-07-13 05:41:23 -0500

    still no good rogues

---
+++ EverCraft.Tests/Rogue_should.cs
@@ -0,0 +1,40 @@
+namespace EverCraft.Tests;
+using Shouldly;
+
+public class Rogue_should
+{
+    [Fact]
+    public void not_be_good_alignment()
+    {
+        Should.Throw<ArgumentException>(() => { ICharacter attacker = new Character() { CharacterClass = CharacterClass.Rogue, Alignment = Alignment.Gud }; });
+    }
+
+    [Fact]
+    public void may_not_be_changed_to_good_alignment()
+    {
+        ICharacter character = new Character() { CharacterClass = CharacterClass.Rogue };
+        Should.Throw<ArgumentException>(() =>
+        {
+            character.Alignment = Alignment.Gud;
+        });
+    }
+
+    [Theory]
+    [InlineData(Alignment.Neutered)]
+    [InlineData(Alignment.Evily)]
+    public void may_be_not_good(Alignment alignment)
+    {
+        ICharacter character = new Character() { CharacterClass = CharacterClass.Rogue, Alignment = alignment };
+        character.Alignment.ShouldBe(alignment);
+    }
+
+    [Theory]
+    [InlineData(Alignment.Neutered)]
+    [InlineData(Alignment.Evily)]
+    public void may_be_not_good_flipped(Alignment alignment)
+    {
+        ICharacter character = new Character() { Alignment = alignment, CharacterClass = CharacterClass.Rogue };
+        character.Alignment.ShouldBe(alignment);
+    }
+
+}
